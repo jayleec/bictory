644d3756cb45da2509c1cfbef658c0bc6a233bda
Eric <spb828@naver.com>
2016-07-18 15:23:10 +0900

    apply template

---
+++ home/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url
+
+from home import views
+
+app_name = 'home'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^tables/$', views.tables, name='tables'),
+    url(r'^forms/$', views.forms, name='forms'),
+    url(r'^forgot_password/$', views.forgot_password, name='forgot_password'),
+]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def forms(request):
+    return render(request, 'forms.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def forgot_password(request):
+    return render(request, 'forgot_password.html')
+
+def tables(request):
+    return render(request, 'tables.html')
